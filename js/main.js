3620f81f093a0e3b5c67a39711fd1604f7109a07
Kirsty <kirsty@microserve.io>
2018-05-09 15:12:26 +0100

    Init

---
+++ js/main.js
@@ -0,0 +1,17 @@
+/**
+ * @file
+ * Main theme js.
+ *
+ * This file will compile into dist/js.
+ */
+
+(function ($) {
+  'use strict';
+
+  // Drupal.behaviors.BEHAVIOUR = {
+  //   attach: function (context, settings) {
+  //     // Code.
+  //   }
+  // };
+
+}(jQuery));
